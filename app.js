83c5f27a950633fe352e2dba3a49aef136d6ad43
Apurva Sawant <apurvasawant45@gmail.com>
2021-05-05 13:10:31 +0530

    Update: Complete Functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("txt-output");
+var txtOutput = document.querySelector("#txt-output");
 var btnTranslate = document.querySelector("#btn-translate")
 
 //Trial translator:
@@ -7,13 +7,25 @@ var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 //Vulcan translator
 // var serverURL="https://api.funtranslations.com/translate/vulcan.json"
 
-function generateURL(){
-    
+function generateURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("Error",error);
+    alert("Something went wrong!! Please try again later.");
 }
 
 
 function clickHandler(){
     var inputText=txtInput.value;
+    fetch(generateURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText=json.contents.translated;
+        txtOutput.innerText=translatedText;
+    })
+    .catch(errorHandler)
 }
 
-btnTranslate.addEventListener(clickHandler());
+btnTranslate.addEventListener("click",clickHandler);
