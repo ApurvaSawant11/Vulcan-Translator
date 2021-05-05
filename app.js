41c7ff09490b2f4333ec299b6c06d2fad8ef6d5e
Apurva Sawant <apurvasawant45@gmail.com>
2021-05-05 13:55:12 +0530

    add: details and credits

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ var txtOutput = document.querySelector("#txt-output");
 var btnTranslate = document.querySelector("#btn-translate")
 
 //Trial translator:
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 //Vulcan translator
-// var serverURL="https://api.funtranslations.com/translate/vulcan.json"
+var serverURL="https://api.funtranslations.com/translate/vulcan.json"
 
 function generateURL(text){
     return serverURL + "?" + "text=" + text
